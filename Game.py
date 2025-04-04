25e4c9fdb375fddadf8ced057a65d2fea59e2ea0
gorkgirk <kamilsiyo@gmail.com>
2025-04-04 18:47:38 +0100

    Create Game.py

---
+++ Game.py
@@ -0,0 +1,19 @@
+import pygame
+import random
+from pygame.locals import *
+
+# Set up Pygame and initialize its modules to prepare for game development.
+pygame.init()
+
+# Define the main game settings like screen dimensions, frame rate, gravity, jump force, and obstacle speed.
+WIDTH, HEIGHT = 800, 600  
+FPS = 60  
+GRAVITY = 0.8  
+JUMP_FORCE = -18  
+OBSTACLE_SPEED = 5  
+# Define color values using RGB format for various elements in the game.
+WHITE = (255, 255, 255)  
+GREEN = (0, 255, 0)  
+VIOLET = (125, 0, 255)  
+BLUE = (0, 0, 255)  
+RED = (255, 0, 0)  
