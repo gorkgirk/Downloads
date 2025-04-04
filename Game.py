7d0cd2dc3be5e301727e99fce1294403f43afda0
gorkgirk <kamilsiyo@gmail.com>
2025-04-04 18:49:38 +0100

    Update Game.py

--- Game.py
+++ Game.py
@@ -11,9 +11,5 @@ FPS = 60
 GRAVITY = 0.8  
 JUMP_FORCE = -18  
 OBSTACLE_SPEED = 5  
-# Define color values using RGB format for various elements in the game.
-WHITE = (255, 255, 255)  
-GREEN = (0, 255, 0)  
-VIOLET = (125, 0, 255)  
-BLUE = (0, 0, 255)  
-RED = (255, 0, 0)  
+
+  
